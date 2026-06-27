80f587585759e6d7beeaa2fa953e8c234d53b118
ahmedWa1408 <aa1229550@gmail.com>
2026-06-27 08:33:36 +0300

    Add multiple plans with details and maps

--- data/plans.js
+++ data/plans.js
@@ -23,4 +23,211 @@ const PLAN4 = [];
 
 const PLAN5 = [];
 
-const PLAN6 = [];
+const PLAN6 = [];/*
+=========================================
+مسار (1)
+عنيزة والمذنب
+=========================================
+*/
+
+PLAN1.push(
+
+{
+code:"QSMSM204-205",
+name:"ثلاث فلاشات",
+storage:"Flash x3",
+xml:true,
+map:"https://maps.app.goo.gl/rD6qtCLzpYay22AN6?g_st=ipc"
+},
+
+{
+code:"113-114",
+name:"موقع 113-114",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/ScMENYkk7k7s62KR7?g_st=iw"
+},
+
+{
+code:"139",
+name:"موقع 139",
+storage:"Hard Disk",
+xml:true,
+map:"https://www.google.com/maps/place/26%C2%B007'31.1%22N+43%C2%B057'32.8%22E/@26.1253188,43.9616809,652m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d26.125314!4d43.959106?entry=ttu"
+},
+
+{
+code:"100",
+name:"موقع 100",
+storage:"Flash",
+xml:true,
+map:"https://goo.gl/maps/FLZCuf1CWLq3DhHz5"
+},
+
+{
+code:"UMZSM194",
+name:"UMZSM194",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/ucDGXCynoh2dbw1H9?g_st=iw"
+},
+
+{
+code:"QSMTE149-150",
+name:"QSMTE149-150",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/XAutYAd5ZtNnaU43A?g_st=ic"
+}
+
+);PLAN1.push(
+
+{
+code:"188-189",
+name:"موقع 188-189",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/j9T84niBqVaWgSrC8?g_st=iw"
+},
+
+{
+code:"QSMIT149-150",
+name:"هارديسكين",
+storage:"Hard Disk x2",
+xml:true,
+map:"https://maps.app.goo.gl/ui7hjoWeNq1VtUAz5?g_st=iw"
+},
+
+{
+code:"178-179",
+name:"موقع 178-179",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/zvDwCJzyKpaD3cMx5?g_st=iw"
+},
+
+{
+code:"67-68",
+name:"موقع 67-68",
+storage:"Flash",
+xml:true,
+map:"https://goo.gl/maps/G33sQ4qWF3iL8jJt8"
+},
+
+{
+code:"QSMTE137-138",
+name:"QSMTE137-138",
+storage:"Flash",
+xml:true,
+map:"https://goo.gl/maps/Afaund5xT2LuUytt6"
+},
+
+{
+code:"176-177",
+name:"موقع 176-177",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/k8qh4Nap8tQRKJFY6?g_st=iw"
+}
+
+);PLAN1.push(
+
+{
+code:"255-256",
+name:"موقع 255-256",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/htN3LhE4hQZg8q8k7?g_st=iw"
+},
+
+{
+code:"UNZSM197-198",
+name:"UNZSM197-198",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/nqg5SNu4DyFSeDvU7?g_st=ipc"
+},
+
+{
+code:"QSMIT001",
+name:"انعطاف",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/sezps3xvYsuCv5fs8?g_st=iw"
+},
+
+{
+code:"QSMIT021",
+name:"إشارة الساعة",
+storage:"Hard Disk",
+xml:true,
+map:"https://maps.app.goo.gl/FgL3dnSySX2woWi99?g_st=iw"
+},
+
+{
+code:"QSMIT006",
+name:"إشارة الساعة",
+storage:"Hard Disk",
+xml:true,
+map:"https://goo.gl/maps/RWUR6vBNXvP6Rxg9A"
+},
+
+{
+code:"QSMIT033",
+name:"مقابل 144",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/9GfrebVeNdhZFAEm6?g_st=iw"
+}
+
+);PLAN1.push(
+
+{
+code:"QSMIT144",
+name:"انعطاف 144",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/JLqtfxFDuQG3Cjdy9?g_st=iw"
+},
+
+{
+code:"143",
+name:"موقع 143",
+storage:"Hard Disk",
+xml:true,
+map:"https://maps.app.goo.gl/8B4kJev31yEiM9ef7?g_st=iw"
+},
+
+{
+code:"QSMSM328-329",
+name:"QSMSM328-329",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/JHXSuswzQQ2FwFFa8?g_st=ipc"
+},
+
+{
+code:"QSMSM326-327",
+name:"QSMSM326-327",
+storage:"Flash",
+xml:true,
+map:"https://maps.app.goo.gl/5wa7NHV1EfEj3Msw9?g_st=ipc"
+},
+
+{
+code:"MTNSM003-004",
+name:"ثلاث فلاشات",
+storage:"Flash x3",
+xml:true,
+map:"https://www.google.com/maps/place/25%C2%B055'54.4%22N+44%C2%B006'59.0%22E/@25.9316716,44.1169308,194m/data=!3m1!1e3!4m4!3m3!8m2!3d25.931778!4d44.116378?entry=ttu"
+},
+
+{
+code:"MTNCP001-002",
+name:"ثلاث فلاشات",
+storage:"Flash x3",
+xml:true,
+map:"https://maps.app.goo.gl/tCaqrGE3bAbuWHoE8?g_st=iw"
+}
+
+);
